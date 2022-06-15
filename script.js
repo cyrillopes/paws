05dac121b6354252b8f132922fbabbb1e12f55ce
cyrillo1 <cyrillopez05@gmail.com>
2022-06-15 14:00:49 +0530

    thrid commit

--- script.js
+++ script.js
@@ -8,16 +8,20 @@ const fonts = document.querySelectorAll(".font-cond");
 const information = document.querySelector(".information_section");
 const shopInfo = document.querySelector(".shop-info");
 const item_collection = document.querySelectorAll(".toys-collection-item");
+const bodyDark = document.querySelector(".body_dark");
 darkMode.addEventListener("click", () => {
-  shopInfo.classList.add("allDark");
-  item_collection.forEach((e) => e.classList.add("card_Color"));
-  body.classList.add("allDark");
   card.forEach((e) => {
-    e.classList.add("card_border");
-    e.classList.add("allDark");
+    e.classList.toggle("card_border");
+    e.classList.toggle("allDark");
   });
-  jumbotron.classList.add("allDark");
-  information.classList.add("allDark");
-  fonts.forEach((e) => e.classList.add("reviews_list"));
-  service.classList.add("allDark");
+  jumbotron.classList.toggle("allDark");
+  information.classList.toggle("allDark");
+  fonts.forEach((e) => e.classList.toggle("reviews_list"));
+  service.classList.toggle("allDark");
+  bodyDark.classList.toggle("allDark");
+});
+darkMode.addEventListener("click", () => {
+  body.classList.toggle("allDark");
+  shopInfo.classList.toggle("allDark");
+  item_collection.forEach((e) => e.classList.toggle("card_Color"));
 });
