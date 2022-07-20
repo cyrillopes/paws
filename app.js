90821c212a156143a6eb57b474d97d9078b527f5
cyrillo1 <cyrillopez05@gmail.com>
2022-07-20 18:15:16 +0530

    Dynamic dropdown

---
+++ app.js
@@ -0,0 +1,95 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+// const cors = require("cors");
+let port = process.env.PORT || 3000;
+const mongoDB = require("mongodb");
+const mongoClient = mongoDB.MongoClient;
+const mongoUrl = process.env.MONGOURL;
+let db;
+
+// app.use(morgan("common"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+// app.use(cors());
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+  // res.send("hello");
+});
+
+app.get("/location", (req, res) => {
+  db.collection("location")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+app.get("/shop", (req, res) => {
+  db.collection("shop")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+app.get("/shop/:category_id", (req, res) => {
+  let query = {};
+  let sort = { price: 1 };
+  let categoryId = Number(req.params.category_id);
+  let brand = req.query.brand;
+  let lcost = Number(req.query.lcost);
+  let hcost = Number(req.query.hcost);
+
+  if (req.query.sort) {
+    sort = { price: req.query.sort };
+  }
+  if (brand && lcost && hcost) {
+    query = {
+      category_id: categoryId,
+      $and: [{ price: { $gt: lcost, $lt: hcost } }],
+      brand: brand,
+    };
+  } else if (lcost && hcost) {
+    query = {
+      category_id: categoryId,
+      $and: [{ price: { $gt: lcost, $lt: hcost } }],
+    };
+  } else if (brand) {
+    query = { brand: brand };
+  } else {
+    query = { category_id: categoryId };
+  }
+  db.collection("shop")
+    .find(query)
+    .sort(sort)
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+app.get("/details/:category_id/:id", (req, res) => {
+  let categoryId = Number(req.params.category_id);
+  let id = Number(req.params.id);
+  db.collection("shop")
+    .find({ category_id: categoryId, id: id })
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+mongoClient.connect(mongoUrl, (err, client) => {
+  if (err) console.log(err);
+  db = client.db("paws");
+  app.listen(3000, () => {
+    console.log("Listening on port 3001");
+  });
+});
---
+++ js/locations.js
@@ -0,0 +1,66 @@
+const locationUrl = "https://api.npoint.io/a66723bb366d82c47202";
+const locationEl = document.querySelector(".search_location");
+const districts = document.querySelector(".search_district");
+
+const callLocation = async function (url) {
+  const location = await fetch(url);
+  const data = await location.json();
+
+  data.map((location) => {
+    let markup = document.createElement("option");
+    let stateName = document.createTextNode(location.state);
+    markup.appendChild(stateName);
+    markup.value = location.state_id;
+    locationEl.appendChild(markup);
+  });
+};
+
+console.log(locationEl.value);
+const callRest = async function () {
+  let locationValue = Number(locationEl.value) - 1;
+  if (districts.length > 0) districts.innerHTML = "";
+
+  const restaurant = await fetch(
+    `https://api.npoint.io/a66723bb366d82c47202/${locationValue}/districts`
+  );
+  const data = await restaurant.json();
+  data.map((rest) => {
+    console.log(rest);
+    let markup = document.createElement("option");
+    let restName = document.createTextNode(rest);
+    console.log(restName);
+
+    markup.appendChild(restName);
+    districts.appendChild(markup);
+  });
+};
+callLocation(locationUrl);
+locationEl.onchange = callRest;
+
+// const searchLocations = document.querySelector(".search_location");
+// const locations = document.querySelector(".location");
+// const results = document.querySelector(".results");
+// let searchable = ["hello", "bye"];
+// searchLocations.addEventListener("keyup", (e) => {
+//   // console.log(e.target.value);
+//   let results = [];
+//   let input = searchLocations.value;
+//   if (input.length) {
+//     results = searchable.filter((item) => {
+//       return item.toLowerCase().includes(input.toLowerCase());
+//     });
+//   }
+//   renderResults(results);
+// });
+
+// const renderResults = function (results) {
+//   if (!results.length) return searchLocations.classList.remove("show");
+
+//   let content = results
+//     .map((item) => {
+//       return `<li>${item}</li>`;
+//     })
+//     .join("");
+//   locations.classList.add("show");
+//   results.innerHTML = `<ul>${content}</ul>`;
+// };
